23720643c7cb17fbdda25a9165dc0de65516c137
Tho Nguyen <ThoND@macs-macbook-pro-2.local>
2016-05-14 01:03:11 +0700

    Initial Commit

---
+++ Video1_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Video1_Swift
+//
+//  Created by Nguyen Duc Tho on 5/14/16.
+//  Copyright © 2016 Nguyen Duc Tho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
